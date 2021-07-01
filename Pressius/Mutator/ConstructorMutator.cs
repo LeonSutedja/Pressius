41775d1b26d61de0379f2d5561d9d667c27e20d9
Leon Sutedja <leon.sutedja@gmail.com>
2021-07-02 06:09:00 +1000

    Small clean up.
    Change constructor mutator and properties mutator as an internal class instead of a partial class of a MutatorFactory

--- Pressius/Mutator/ConstructorMutator.cs
+++ Pressius/Mutator/ConstructorMutator.cs
@@ -6,141 +6,138 @@ using System.Reflection;
 
 namespace Pressius
 {
-    internal partial class MutatorFactory
+    internal class ConstructorMutator
     {
-        private class ConstructorMutator
+        private readonly Type _classType;
+        private readonly IObjectDefinition _objectDefinition;
+        private readonly List<IParameterDefinition> _parameterDefinitions;
+        private readonly string _idParameterName;
+
+        public ConstructorMutator(
+            Type classType,
+            IObjectDefinition objectDefinition,
+            List<IParameterDefinition> parameterDefinitions,
+            string idParameterName)
         {
-            private readonly Type _classType;
-            private readonly IObjectDefinition _objectDefinition;
-            private readonly List<IParameterDefinition> _parameterDefinitions;
-            private readonly string _idParameterName;
+            _classType = classType;
+            _objectDefinition = objectDefinition;
+            _parameterDefinitions = parameterDefinitions;
+            _idParameterName = idParameterName;
+        }
 
-            public ConstructorMutator(
-                Type classType,
-                IObjectDefinition objectDefinition,
-                List<IParameterDefinition> parameterDefinitions,
-                string idParameterName)
+        public IEnumerable<object> Mutate()
+        {
+            // Default, get the first constructor.
+            var classConstructors = _classType.GetConstructors().FirstOrDefault();
+            if (classConstructors == null)
+                throw new Exception($"Cannot found any constructor for this class type: {_classType.Name}");
+            var listOfParameters = classConstructors.GetParameters();
+
+            var propertyPermutationLists = _generatePermutationList(listOfParameters,
+                _objectDefinition, _parameterDefinitions);
+
+            var attributePermutations = new List<List<object>>();
+            propertyPermutationLists.GeneratePermutations(attributePermutations);
+
+            var attributePermutationsWithId = _generateAttributePermutationsId(listOfParameters,
+                attributePermutations, _idParameterName);
+
+            var results = attributePermutations
+                .Select(ap => Activator.CreateInstance(_classType, ap.ToArray()));
+
+            return results;
+        }
+
+        private List<List<object>> _generatePermutationList(
+            ParameterInfo[] listOfParameters,
+            IObjectDefinition objectDefinition,
+            List<IParameterDefinition> parameterDefinitions
+            )
+        {
+            var propertyPermutationLists = new List<List<object>>();
+            foreach (var param in listOfParameters)
             {
-                _classType = classType;
-                _objectDefinition = objectDefinition;
-                _parameterDefinitions = parameterDefinitions;
-                _idParameterName = idParameterName;
-            }
+                var inputDefinitionType = string.Empty;
+                var attributeParameterNameDefinitions = parameterDefinitions.Where(pd => pd.CompareParamName).ToList();
 
-            public IEnumerable<object> Mutate()
-            {
-                // Default, get the first constructor.
-                var classConstructors = _classType.GetConstructors().FirstOrDefault();
-                if (classConstructors == null)
-                    throw new Exception($"Cannot found any constructor for this class type: {_classType.Name}");
-                var listOfParameters = classConstructors.GetParameters();
-
-                var propertyPermutationLists = _generatePermutationList(listOfParameters,
-                    _objectDefinition, _parameterDefinitions);
-
-                var attributePermutations = new List<List<object>>();
-                propertyPermutationLists.GeneratePermutations(attributePermutations);
-
-                var attributePermutationsWithId = _generateAttributePermutationsId(listOfParameters,
-                    attributePermutations, _idParameterName);
-
-                var results = attributePermutations
-                    .Select(ap => Activator.CreateInstance(_classType, ap.ToArray()));
-
-                return results;
-            }
-
-            private List<List<object>> _generatePermutationList(
-                ParameterInfo[] listOfParameters,
-                IObjectDefinition objectDefinition,
-                List<IParameterDefinition> parameterDefinitions
-                )
-            {
-                var propertyPermutationLists = new List<List<object>>();
-                foreach (var param in listOfParameters)
+                if (objectDefinition == null)
                 {
-                    var inputDefinitionType = string.Empty;
-                    var attributeParameterNameDefinitions = parameterDefinitions.Where(pd => pd.CompareParamName).ToList();
-
-                    if (objectDefinition == null)
+                    if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(param.Name)))
                     {
-                        if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(param.Name)))
-                        {
-                            inputDefinitionType = attributeParameterNameDefinitions
-                                .First(pd => pd.TypeName.Equals(param.Name)).TypeName.NameList.First();
-                        }
-                        else
-                        {
-                            inputDefinitionType = param.IsNullableParameter()
-                                ? param.GetNullableParameterName()
-                                : param.ParameterType.Name;
-                        }
+                        inputDefinitionType = attributeParameterNameDefinitions
+                            .First(pd => pd.TypeName.Equals(param.Name)).TypeName.NameList.First();
                     }
-                    else if (!objectDefinition.MatcherDictionary.TryGetValue(param.Name, out inputDefinitionType))
-                    {
-                        if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(param.Name)))
-                        {
-                            inputDefinitionType = attributeParameterNameDefinitions
-                                .First(pd => pd.TypeName.Equals(param.Name)).TypeName.NameList.First();
-                        }
-                        else
-                        {
-                            inputDefinitionType = param.IsNullableParameter()
-                                ? param.GetNullableParameterName()
-                                : param.ParameterType.Name;
-                        }
-                    }
-
-                    var testInput = parameterDefinitions.FirstOrDefault(
-                        id => id.TypeName.Equals(inputDefinitionType))?.InputCatalogues;
-
-                    if(string.Equals(param.ParameterType.Name, "Guid"))
-                        testInput = new List<object> { null };
-
-                    if (testInput == null)
-                        throw new Exception($"Cannot Found Matching Definition for: { inputDefinitionType }, have you assign ObjectDefinition correctly?");
-
-                    propertyPermutationLists.Add(testInput);
-                }
-
-                return propertyPermutationLists;
-            }
-
-            private List<List<object>> _generateAttributePermutationsId(
-                ParameterInfo[] listOfParameters,
-                List<List<object>> currentAttributePermutations,
-                string idParameterName)
-            {
-                ParameterInfo parameter;
-                if(_idParameterName == null)
-                    parameter = listOfParameters.FirstOrDefault(pd => string.Equals(pd.ParameterType.Name, "Guid"));
-                else
-                    parameter = listOfParameters.FirstOrDefault(pd => string.Equals(pd.Name, idParameterName));
-                
-                if(parameter == null)
-                {                   
-                    if (idParameterName != null)
-                        throw new Exception("Cannot find id with attribute name: " + idParameterName);
-                    return currentAttributePermutations;
-                }
-
-                var idIndex = listOfParameters.ToList().IndexOf(parameter);
-
-                var count = 0;
-                var idType = parameter.ParameterType.Name;
-                foreach (var attributePermutation in currentAttributePermutations)
-                {
-                    count++;
-                    if (idType.Contains("Guid"))
-                        attributePermutation[idIndex] = Guid.NewGuid();
-                    else if(idType.Contains("Int"))
-                        attributePermutation[idIndex] = count;
                     else
-                        throw new Exception("Only Int and Guid is supported for Id generator");                    
+                    {
+                        inputDefinitionType = param.IsNullableParameter()
+                            ? param.GetNullableParameterName()
+                            : param.ParameterType.Name;
+                    }
+                }
+                else if (!objectDefinition.MatcherDictionary.TryGetValue(param.Name, out inputDefinitionType))
+                {
+                    if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(param.Name)))
+                    {
+                        inputDefinitionType = attributeParameterNameDefinitions
+                            .First(pd => pd.TypeName.Equals(param.Name)).TypeName.NameList.First();
+                    }
+                    else
+                    {
+                        inputDefinitionType = param.IsNullableParameter()
+                            ? param.GetNullableParameterName()
+                            : param.ParameterType.Name;
+                    }
                 }
 
+                var testInput = parameterDefinitions.FirstOrDefault(
+                    id => id.TypeName.Equals(inputDefinitionType))?.InputCatalogues;
+
+                if (string.Equals(param.ParameterType.Name, "Guid"))
+                    testInput = new List<object> { null };
+
+                if (testInput == null)
+                    throw new Exception($"Cannot Found Matching Definition for: { inputDefinitionType }, have you assign ObjectDefinition correctly?");
+
+                propertyPermutationLists.Add(testInput);
+            }
+
+            return propertyPermutationLists;
+        }
+
+        private List<List<object>> _generateAttributePermutationsId(
+            ParameterInfo[] listOfParameters,
+            List<List<object>> currentAttributePermutations,
+            string idParameterName)
+        {
+            ParameterInfo parameter;
+            if (_idParameterName == null)
+                parameter = listOfParameters.FirstOrDefault(pd => string.Equals(pd.ParameterType.Name, "Guid"));
+            else
+                parameter = listOfParameters.FirstOrDefault(pd => string.Equals(pd.Name, idParameterName));
+
+            if (parameter == null)
+            {
+                if (idParameterName != null)
+                    throw new Exception("Cannot find id with attribute name: " + idParameterName);
                 return currentAttributePermutations;
-            }            
+            }
+
+            var idIndex = listOfParameters.ToList().IndexOf(parameter);
+
+            var count = 0;
+            var idType = parameter.ParameterType.Name;
+            foreach (var attributePermutation in currentAttributePermutations)
+            {
+                count++;
+                if (idType.Contains("Guid"))
+                    attributePermutation[idIndex] = Guid.NewGuid();
+                else if (idType.Contains("Int"))
+                    attributePermutation[idIndex] = count;
+                else
+                    throw new Exception("Only Int and Guid is supported for Id generator");
+            }
+
+            return currentAttributePermutations;
         }
     }
 }
--- Pressius/Mutator/PropertiesMutator.cs
+++ Pressius/Mutator/PropertiesMutator.cs
@@ -6,148 +6,148 @@ using System.Reflection;
 
 namespace Pressius
 {
-    internal partial class MutatorFactory
+    internal class PropertiesMutator
     {
-        private class PropertiesMutator
+        private readonly string _idName;
+        private readonly Type _classType;
+        private readonly IObjectDefinition _objectDefinition;
+        private readonly List<IParameterDefinition> _parameterDefinitions;
+
+        public PropertiesMutator(
+            string idName,
+            Type classType,
+            IObjectDefinition objectDefinition,
+            List<IParameterDefinition> parameterDefinitions)
         {
-            private readonly string _idName;
-            private readonly Type _classType;
-            private readonly IObjectDefinition _objectDefinition;
-            private readonly List<IParameterDefinition> _parameterDefinitions;
+            _idName = idName;
+            _classType = classType;
+            _objectDefinition = objectDefinition;
+            _parameterDefinitions = parameterDefinitions;
+        }
 
-            public PropertiesMutator(
-                string idName,
-                Type classType,
-                IObjectDefinition objectDefinition,
-                List<IParameterDefinition> parameterDefinitions)
+        public IEnumerable<object> Mutate()
+        {
+            var listOfProperties = _classType.GetProperties();
+            var propertyPermutationLists = _generatePermutationList(
+                _idName,
+                listOfProperties,
+                _parameterDefinitions,
+                _objectDefinition);
+
+            var attributePermutations = new List<List<object>>();
+            propertyPermutationLists.GeneratePermutations(attributePermutations);
+            return _generateObjectList(_idName, _classType, attributePermutations);
+        }
+
+        private List<List<object>> _generatePermutationList(
+            string idName,
+            PropertyInfo[] propertyInfos,
+            List<IParameterDefinition> parameterDefinitions,
+            IObjectDefinition objectDefinition)
+        {
+            var propertyPermutationLists = new List<List<object>>();
+            var attributeParameterNameDefinitions = parameterDefinitions.Where(pd => pd.CompareParamName).ToList();
+            foreach (var prop in propertyInfos)
             {
-                _idName = idName;
-                _classType = classType;
-                _objectDefinition = objectDefinition;
-                _parameterDefinitions = parameterDefinitions;
-            }
+                var inputDefinitionType = string.Empty;
 
-            public IEnumerable<object> Mutate()
-            {
-                var listOfProperties = _classType.GetProperties();
-                var propertyPermutationLists = _generatePermutationList(
-                    _idName,
-                    listOfProperties,
-                    _parameterDefinitions,
-                    _objectDefinition);
-
-                var attributePermutations = new List<List<object>>();
-                propertyPermutationLists.GeneratePermutations(attributePermutations);
-                return _generateObjectList(_idName, _classType, attributePermutations);
-            }           
-
-            private List<List<object>> _generatePermutationList(
-                string idName,
-                PropertyInfo[] propertyInfos,
-                List<IParameterDefinition> parameterDefinitions,
-                IObjectDefinition objectDefinition)
-            {
-                var propertyPermutationLists = new List<List<object>>();
-                var attributeParameterNameDefinitions = parameterDefinitions.Where(pd => pd.CompareParamName).ToList();
-                foreach (var prop in propertyInfos)
+                if (objectDefinition == null)
                 {
-                    var inputDefinitionType = string.Empty;                    
-
-                    if (objectDefinition == null)
+                    // when there is no object definition, we will check if there is
+                    // an attribute parameter name definition that matches with the prop name.
+                    // otherwise, we will assign the default prop.PropertyType.Name value
+                    if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(prop.Name)))
                     {
-                        // when there is no object definition, we will check if there is
-                        // an attribute parameter name definition that matches with the prop name.
-                        // otherwise, we will assign the default prop.PropertyType.Name value
-                        if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(prop.Name)))
-                        {
-                            inputDefinitionType = attributeParameterNameDefinitions
-                                .First(pd => pd.TypeName.Equals(prop.Name)).TypeName.NameList.First();
-                        }
-                        else
-                        {
-                            inputDefinitionType = prop.IsNullableProperty()
-                                ? prop.GetNullablePropertyName()
-                                : prop.PropertyType.Name;
-                        }
+                        inputDefinitionType = attributeParameterNameDefinitions
+                            .First(pd => pd.TypeName.Equals(prop.Name)).TypeName.NameList.First();
                     }
-                    else if (!objectDefinition.MatcherDictionary.TryGetValue(prop.Name, out inputDefinitionType))
+                    else
                     {
-                        // when there is an object definition and cannot be matched,
-                        // we will check if there is an attribute parameter name definition
-                        // that matches with the prop name.
-                        // otherwise, we will assign the default prop.PropertyType.Name value
-                        if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(prop.Name)))
-                        {
-                            inputDefinitionType = attributeParameterNameDefinitions
-                                .First(pd => pd.TypeName.Equals(prop.Name)).TypeName.NameList.First();
-                        }
-                        else
-                        {
-                            inputDefinitionType = prop.IsNullableProperty()
-                                ? prop.GetNullablePropertyName()
-                                : prop.PropertyType.Name;
-                        }
+                        inputDefinitionType = prop.IsNullableProperty()
+                            ? prop.GetNullablePropertyName()
+                            : prop.PropertyType.Name;
                     }
+                }
+                else if (!objectDefinition.MatcherDictionary.TryGetValue(prop.Name, out inputDefinitionType))
+                {
+                    // when there is an object definition and cannot be matched,
+                    // we will check if there is an attribute parameter name definition
+                    // that matches with the prop name.
+                    // otherwise, we will assign the default prop.PropertyType.Name value
+                    if (attributeParameterNameDefinitions.Any(pd => pd.TypeName.Equals(prop.Name)))
+                    {
+                        inputDefinitionType = attributeParameterNameDefinitions
+                            .First(pd => pd.TypeName.Equals(prop.Name)).TypeName.NameList.First();
+                    }
+                    else
+                    {
+                        inputDefinitionType = prop.IsNullableProperty()
+                            ? prop.GetNullablePropertyName()
+                            : prop.PropertyType.Name;
+                    }
+                }
 
-                    var testInput = parameterDefinitions.FirstOrDefault(
+                var testInput = new List<object>();
+
+                if (string.Equals(prop.PropertyType.Name, "Guid"))
+                    testInput = new List<object> { null };
+                else
+                    testInput = parameterDefinitions.FirstOrDefault(
                         id => id.TypeName.Equals(inputDefinitionType))?.InputCatalogues;
-                    
-                    if (string.Equals(prop.PropertyType.Name, "Guid"))
-                        testInput = new List<object> { null };
 
-                    if (testInput == null)                    
-                        throw new Exception($"Cannot Found Matching Definition for: { inputDefinitionType }, have you assign ObjectDefinition correctly?");
-                    
-
-                    propertyPermutationLists.Add(testInput);
+                if (testInput == null)
+                {
+                    throw new Exception($"Cannot Found Matching Definition for: { inputDefinitionType }, have you assign ObjectDefinition correctly?");
                 }
 
-                return propertyPermutationLists;
+                propertyPermutationLists.Add(testInput);
             }
 
-            private List<object> _generateObjectList(
-               string idParameterName,
-               Type classType,
-               List<List<object>> attributePermutations)
-            {
-                var listOfProperties = classType.GetProperties();
-                var results = new List<object>();
-                var idCount = 0;
-                foreach(var permutationSet in attributePermutations)
-                {
-                    var constructor = classType.GetConstructor(Type.EmptyTypes);
-                    if(constructor == null)
-                        throw new Exception($"No parameterless constructor in the class: { classType.Name }. Please add parameterless constructor.");
-                    var newInput = Activator.CreateInstance(classType);
-                    for(var i = 0; i < listOfProperties.Length; i++)
-                    {
-                        var prop = listOfProperties[i];
-                        if(idParameterName != null && prop.Name.Equals(idParameterName))
-                        {
-                            idCount++;
-                            if(prop.PropertyType.Name.Contains("Int"))
-                                prop.SetValue(newInput, idCount);
-                            else
-                            {
-                                if(prop.PropertyType.Name.Contains("Guid"))
-                                    throw new Exception("Guid does not require WithId");
+            return propertyPermutationLists;
+        }
 
-                                throw new Exception("Only Int and Guid is supported for Id generator");
-                            }
-                        }
+        private List<object> _generateObjectList(
+           string idParameterName,
+           Type classType,
+           List<List<object>> attributePermutations)
+        {
+            var listOfProperties = classType.GetProperties();
+            var results = new List<object>();
+            var idCount = 0;
+            foreach (var permutationSet in attributePermutations)
+            {
+                var constructor = classType.GetConstructor(Type.EmptyTypes);
+                if (constructor == null)
+                    throw new Exception($"No parameterless constructor in the class: { classType.Name }. Please add parameterless constructor.");
+                var newInput = Activator.CreateInstance(classType);
+                for (var i = 0; i < listOfProperties.Length; i++)
+                {
+                    var prop = listOfProperties[i];
+                    if (idParameterName != null && prop.Name.Equals(idParameterName))
+                    {
+                        idCount++;
+                        if (prop.PropertyType.Name.Contains("Int"))
+                            prop.SetValue(newInput, idCount);
                         else
                         {
-                            var propValue = permutationSet[i];
-                            if(prop.PropertyType.Name.Contains("Guid"))
-                                propValue = Guid.NewGuid();                                
-                            prop.SetValue(newInput, propValue);
+                            if (prop.PropertyType.Name.Contains("Guid"))
+                                throw new Exception("Guid does not require WithId");
+
+                            throw new Exception("Only Int and Guid is supported for Id generator");
                         }
                     }
-                    results.Add(newInput);
+                    else
+                    {
+                        var propValue = permutationSet[i];
+                        if (prop.PropertyType.Name.Contains("Guid"))
+                            propValue = Guid.NewGuid();
+                        prop.SetValue(newInput, propValue);
+                    }
                 }
-
-                return results;
+                results.Add(newInput);
             }
+
+            return results;
         }
     }
 }
